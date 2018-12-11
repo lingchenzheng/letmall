ec2715363dae7898d301123229dbce4799e2de74
lingcz <lingcz@htwy.com>
2018-12-11 15:33:46 +0800

    delet some comments

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const PORT = process.env.PORT || 3000
 //配置跨域
 app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
-    //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
     res.header('Access-Control-Allow-Headers', '*')
     res.header('Access-Control-Allow-Headers', 'Authorization')
     res.header('Access-Control-Allow-Methods', '*')
